b25c8f35d0cae0c01583b660a5c9c9ac86b6e5db
RJ Pablo <numerianop@sedata.com>
2017-08-22 00:17:29 +0800

    Organized Entity classes in folders Added UserToGameRelationship model

---
+++ DribblyAPI/Models/UserToGameRelationship.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DribblyAPI.Models
+{
+    public class UserToGameRelationship
+    {
+
+        public int gameId { get; set; }
+
+        public string userId { get; set; }
+
+        /// <summary>
+        /// Whether or not the user is the creator of the game.
+        /// </summary>
+        public bool isCreator { get; set; }
+
+        /// <summary>
+        /// Whether or not the user has requested to join the game.
+        /// </summary>
+        public bool hasRequested { get; set; }
+
+        /// <summary>
+        /// Whether or not the user has been kicked out of the game. In which case,
+        /// the user will not be allowed to join the game any more.
+        /// </summary>
+        public bool isKicked { get; set; }
+
+        /// <summary>
+        /// Whether or not the user has been approved to play in the game.
+        /// </summary>
+        public bool isPlaying { get; set; }
+    }
+}
